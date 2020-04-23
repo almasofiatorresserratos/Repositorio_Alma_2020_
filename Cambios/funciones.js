c500bec11c6b667de1c70b400bb6b041010ce4fe
almasofiatorresserratos <55114502+almasofiatorresserratos@users.noreply.github.com>
2020-04-23 18:16:45 -0500

    Continuación de lo de Cachis con ayuda de Hiromi

--- Cambios/funciones.js
+++ Cambios/funciones.js
@@ -1,4 +1,28 @@
+let coloreado = false
+let subrayado = false
+
 function cambiarColor() {
   let etiqueta = document.getElementById("aqui")
   etiqueta.classList.add("letrasAqua")
+  if (coloreado == false) {
+    texto.classList.add("letrasAqua")
+    coloreado = true
+  } else {
+    texto.classList.renove("letrasAqua")
+    coloreado = false
+  }
+}
+function Subrayar() {
+  let etiqueta = document.getElementById("aqui")
+  etiqueta.classList.add("subrayar")
+  if (subrayado == false) {
+      texto.classList.add("subrayar")
+      subrayado = true
+    } else {
+      texto.classList.renove("subrayar")
+      subrayado = false
+}
+function pedirNombre() {
+  let nombre = prompt ("¿Que quieres escribir?");
+  document.getElementById("aqui")
 }
