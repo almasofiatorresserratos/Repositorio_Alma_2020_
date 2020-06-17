a8051f93a113994dff0e43e5a37331d74080fad6
almasofiatorresserratos <55114502+almasofiatorresserratos@users.noreply.github.com>
2020-06-16 20:51:34 -0500

    modals

--- Proyecto.js
+++ Proyecto.js
@@ -1,16 +1,71 @@
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+function mostrar() {
+ let modal = document.getElementById("myModal");
+ modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function ocultar() {
+ let modal = document.getElementById("myModal");
+ modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+// When the user clicks on the button, open the modal
+function mostrar2() {
+ let modal = document.getElementById("myModal2");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar2() {
+ let modal = document.getElementById("myModal2");
+ modal.style.display = "none";
+}
+
+// When the user clicks on the button, open the modal
+function mostrar3() {
+ let modal = document.getElementById("myModal3");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar3() {
+ let modal = document.getElementById("myModal3");
+ modal.style.display = "none";
+}
+
+// When the user clicks on the button, open the modal
+function mostrar4() {
+ let modal = document.getElementById("myModal4");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar4() {
+ let modal = document.getElementById("myModal4");
+ modal.style.display = "none";
+}
+
+// When the user clicks on the button, open the modal
+function mostrar5() {
+ let modal = document.getElementById("myModal5");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar5() {
+ let modal = document.getElementById("myModal5");
+ modal.style.display = "none";
+}
+
+// When the user clicks on the button, open the modal
+function mostrar6() {
+ let modal = document.getElementById("myModal6");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar6() {
+ let modal = document.getElementById("myModal6");
+ modal.style.display = "none";
 }
