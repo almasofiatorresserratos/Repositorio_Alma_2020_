5a31d92a9a584d7466fe8657acc2265ef1e1668e
almasofiatorresserratos <55114502+almasofiatorresserratos@users.noreply.github.com>
2020-07-01 22:37:02 -0500

    Nueva pagina

---
+++ LOTR.js
@@ -0,0 +1,65 @@
+// When the user clicks on the button, open the modal
+function mostrar() {
+ let modal = document.getElementById("myModal");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar() {
+ let modal = document.getElementById("myModal");
+ modal.style.display = "none";
+}
+
+
+// When the user clicks on the button, open the modal
+function mostrar2() {
+ let modal = document.getElementById("myModal2");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar2() {
+ let modal = document.getElementById("myModal2");
+ modal.style.display = "none";
+}
+
+
+// When the user clicks on the button, open the modal
+function mostrar3() {
+ let modal = document.getElementById("myModal3");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar3() {
+ let modal = document.getElementById("myModal3");
+ modal.style.display = "none";
+}
+
+
+
+// When the user clicks on the button, open the modal
+function mostrar4() {
+ let modal = document.getElementById("myModal4");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar4() {
+ let modal = document.getElementById("myModal4");
+ modal.style.display = "none";
+}
+
+
+
+// When the user clicks on the button, open the modal
+function mostrar7() {
+ let modal = document.getElementById("myModal7");
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar7() {
+ let modal = document.getElementById("myModal7");
+ modal.style.display = "none";
+}
