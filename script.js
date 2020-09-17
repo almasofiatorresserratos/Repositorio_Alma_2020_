47055f457bfc898435d387e54a27245eaf84501c
almasofiatorresserratos <55114502+almasofiatorresserratos@users.noreply.github.com>
2020-09-17 18:12:32 -0500

    Cambios de ganar

--- script.js
+++ script.js
@@ -12,27 +12,58 @@ if(!conAlgo){
     casilla.classList.add("casillaX");
     casilla.classList.remove("casillaO");
     casilla.childNodes[0].innerText= "X";
-    copiarTablero[numero] = "X";
+    copiarTablero[numero - 1] = "X";
     turnoJugador = false
 }
 else {
   casilla.classList.add("casillaO")
   casilla.classList.remove("casillaX");
   casilla.childNodes[0].innerText = "0";
-  copiarTablero[numero] = "0";
+  copiarTablero[numero-1] = "0";
   turnoJugador = true
+    }
+  if (revisarGanar()) {
+    console.log("¡Ya hay ganador!");
+  }
+  }
+//console.log(copiarTablero);
 }
-}
+function revisarGanar() {
+  if (
+      (copiarTablero[0] && copiarTablero[0] == copiarTablero[3] && copiarTablero[0] == copiarTablero [6]) ||
+      (copiarTablero[1] && copiarTablero[1] == copiarTablero[4] && copiarTablero[1] == copiarTablero [7]) ||
+      (copiarTablero[2] && copiarTablero[2] == copiarTablero[5] && copiarTablero[2] == copiarTablero [8])
+      )  {
+  console.log("Vertical");
+  return true;
+  }
+
+  if (
+    (copiarTablero[0] && copiarTablero[0] == copiarTablero[1] && copiarTablero[0] == copiarTablero [2]) ||
+    (copiarTablero[3] && copiarTablero[3] == copiarTablero[4] && copiarTablero[3] == copiarTablero [5]) ||
+    (copiarTablero[6] && copiarTablero[6] == copiarTablero[7] && copiarTablero[6] == copiarTablero [8])
+    ) {
+      console.log("Horizontal");
+      return true;
+  }
+
+  if (
+    (copiarTablero[0] && copiarTablero[0] == copiarTablero[4] && copiarTablero[0] == copiarTablero [8]) ||
+    (copiarTablero[2] && copiarTablero[2] == copiarTablero[4] && copiarTablero[2] == copiarTablero [6])
+  ) {
+    console.log("Diagonal")
+    return true;
+  }
 }
 
 
 function ocupada(casilla) {
   if (casilla.childNodes[0].innerText){
-    console.log("sí hay algo");
+    //console.log("sí hay algo");
     return true;
   }
   else {
-    console.log("no hay nada")
+    //console.log("no hay nada")
     return false;
   }
 }
