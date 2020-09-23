15b034be2581af3e4bc1423e2bd1c077a2338aa7
almasofiatorresserratos <55114502+almasofiatorresserratos@users.noreply.github.com>
2020-09-22 19:48:01 -0500

    Efectos y cambios

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 console.log("hola")
 let turnoJugador = true
 let copiarTablero = []
+let seguirJugando = true;
 
 function marcar(numero) {
   let casilla = document.getElementById("casilla" + numero);
+  let conAlgo = ocupada(casilla)
 
-let conAlgo = ocupada(casilla)
-
-if(!conAlgo){
-  if(turnoJugador){
-    casilla.classList.add("casillaX");
-    casilla.classList.remove("casillaO");
-    casilla.childNodes[0].innerText= "X";
-    copiarTablero[numero - 1] = "X";
-    turnoJugador = false
-}
-else {
-  casilla.classList.add("casillaO")
-  casilla.classList.remove("casillaX");
-  casilla.childNodes[0].innerText = "0";
-  copiarTablero[numero-1] = "0";
-  turnoJugador = true
+if (seguirJugando) {
+  if(!conAlgo){
+    if (turnoJugador) {
+      casilla.classList.add("casillaX");
+      casilla.classList.remove("casillaO");
+      casilla.childNodes[0].innerText= "X";
+      copiarTablero[numero - 1] = "X";
+      turnoJugador = false
+  }
+  else {
+    casilla.classList.add("casillaO")
+    casilla.classList.remove("casillaX");
+    casilla.childNodes[0].innerText = "0";
+    copiarTablero[numero-1] = "0";
+    turnoJugador = true
+      }
+    if (revisarGanar()) {
+      console.log("¡Ya hay ganador!");
+      seguirJugando = false
+      document.getElementById("felicidades").style.display = "inline-block";
     }
-  if (revisarGanar()) {
-    console.log("¡Ya hay ganador!");
-  }
-  }
+    }
+}
 //console.log(copiarTablero);
 }
 function revisarGanar() {
@@ -34,7 +38,7 @@ function revisarGanar() {
       (copiarTablero[1] && copiarTablero[1] == copiarTablero[4] && copiarTablero[1] == copiarTablero [7]) ||
       (copiarTablero[2] && copiarTablero[2] == copiarTablero[5] && copiarTablero[2] == copiarTablero [8])
       )  {
-  console.log("Vertical");
+  //console.log("Vertical");
   return true;
   }
 
@@ -43,7 +47,7 @@ function revisarGanar() {
     (copiarTablero[3] && copiarTablero[3] == copiarTablero[4] && copiarTablero[3] == copiarTablero [5]) ||
     (copiarTablero[6] && copiarTablero[6] == copiarTablero[7] && copiarTablero[6] == copiarTablero [8])
     ) {
-      console.log("Horizontal");
+      //console.log("Horizontal");
       return true;
   }
 
@@ -51,7 +55,7 @@ function revisarGanar() {
     (copiarTablero[0] && copiarTablero[0] == copiarTablero[4] && copiarTablero[0] == copiarTablero [8]) ||
     (copiarTablero[2] && copiarTablero[2] == copiarTablero[4] && copiarTablero[2] == copiarTablero [6])
   ) {
-    console.log("Diagonal")
+    //console.log("Diagonal")
     return true;
   }
 }
@@ -79,6 +83,8 @@ function rei() {
   limpiarCasilla (8);
   limpiarCasilla (9);
   copiarTablero = []
+  seguirJugando = true;
+  document.getElementById("felicidades").style.display = "none";
 }
 function limpiarCasilla(numero) {
   let casilla = document.getElementById ("casilla" + numero);
