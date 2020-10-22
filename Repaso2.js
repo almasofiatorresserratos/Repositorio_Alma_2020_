31fd5e63743d47218e24a9c5b293f2a8d5925e7b
almasofiatorresserratos <55114502+almasofiatorresserratos@users.noreply.github.com>
2020-10-22 18:10:14 -0500

    Inicio funciones

--- Repaso2.js
+++ Repaso2.js
@@ -1,3 +1,29 @@
-function roca() {
-  let piedra = getElementById("roca");
+function jugar(opUsuario) {
+  console.log(opUsuario);
+  let opCPU = Math.floor(Math.random() * 3) + 1;
+  console.log(opCPU);
+}
+//Revisar si gana el jugador o el CPU:
+function revPuntos() {
+  if (
+      (opUsuario == opCPU)
+      )  {
+        document.getElementById(em);
+  }
+
+  if (
+    (opUsuario[1] && opCPU[2]) ||
+    (opUsuario[2] && opCPU[3]) ||
+    (opUsuario[3] && opCPU[1])
+    ) {
+      document.getElementById(gcpu);
+  }
+
+  if (
+    (opUsuario[1] && opCPU[3]) ||
+    (opUsuario[2] && opCPU[1]) ||
+    (opUsuario[3] && opCPU[2])
+  ) {
+    document.getElementById(gjug);
+  }
 }
