4271549ab7195dbe9ded8a131fdbe936c1196479
almasofiatorresserratos <55114502+almasofiatorresserratos@users.noreply.github.com>
2020-10-29 18:33:12 -0600

    Cambios para finalizar juego

--- Repaso2.js
+++ Repaso2.js
@@ -1,29 +1,44 @@
+let pusu = 0
+let pcom = 0
 function jugar(opUsuario) {
   console.log(opUsuario);
   let opCPU = Math.floor(Math.random() * 3) + 1;
   console.log(opCPU);
+  revPuntos(opUsuario, opCPU)
 }
 //Revisar si gana el jugador o el CPU:
-function revPuntos() {
+function revPuntos(opUsuario, opCPU) {
   if (
-      (opUsuario == opCPU)
+      opUsuario == opCPU
       )  {
-        document.getElementById(em);
+        document.getElementById("em").style.display = "block";
+        document.getElementById("gcpu").style.display = "none";
+        document.getElementById("gjug").style.display = "none";
+
+
+
   }
 
   if (
-    (opUsuario[1] && opCPU[2]) ||
-    (opUsuario[2] && opCPU[3]) ||
-    (opUsuario[3] && opCPU[1])
+    (opUsuario == 1 && opCPU == 2) ||
+    (opUsuario == 2 && opCPU == 3) ||
+    (opUsuario == 3 && opCPU == 1)
     ) {
-      document.getElementById(gcpu);
+      document.getElementById("gcpu").style.display = "block";
+      document.getElementById("em").style.display = "none";
+      document.getElementById("gjug").style.display = "none";
+      document.getElementById("pc").innerText = pcom += 1;
+
   }
 
   if (
-    (opUsuario[1] && opCPU[3]) ||
-    (opUsuario[2] && opCPU[1]) ||
-    (opUsuario[3] && opCPU[2])
+    (opUsuario == 1 && opCPU == 3) ||
+    (opUsuario == 2 && opCPU == 1) ||
+    (opUsuario == 3 && opCPU == 2)
   ) {
-    document.getElementById(gjug);
+    document.getElementById("gjug").style.display = "block";
+    document.getElementById("gcpu").style.display = "none";
+    document.getElementById("em").style.display = "none";
+    document.getElementById("pj").innerText = pusu += 1;
   }
 }
